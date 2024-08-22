eba6ebf463ea9902d49818b9b575947757639ec3
pranav <pranavkulkarni014@gmail.com>
2024-08-22 08:33:34 +0530

    committing my first change of the day

---
+++ test1.java
@@ -0,0 +1,21 @@
+class test
+{
+
+public int add()
+{
+	float sum=0;
+	int a=2, b=2;
+	
+	sum=a+b;
+	System.out.println("printing sum:::"+sum);
+}
+
+
+public static void main (String args[])
+{
+
+  test obj1=new test();	
+  obj1.add();
+}
+
+}
